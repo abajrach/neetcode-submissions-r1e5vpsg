31a5b8b901f3cf7cb5b0f37fd5205171a1507f02
abajrach <arbindrabajracharya@gmail.com>
2026-03-31 21:10:30 -0700

    Add: single-number - submission-2

---
+++ Data Structures & Algorithms/single-number/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0;
+
+        while (i < nums.length - 1) {
+            if (nums[i] == nums[i + 1]) {
+                i += 2;
+            } else {
+                return nums[i];
+            }
+        }
+
+        return nums[i];
+    }
+}
