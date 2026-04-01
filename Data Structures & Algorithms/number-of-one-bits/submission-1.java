a29c06bcb28ebc3ded37213b3cbea8ee9937af5c
abajrach <arbindrabajracharya@gmail.com>
2026-03-31 21:31:30 -0700

    Add: number-of-one-bits - submission-1

---
+++ Data Structures & Algorithms/number-of-one-bits/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int ones = 0;
+        while (n != 0) {
+
+            // Modding by 2 tells if the rightmost digit of a number is 1 or 0.
+            // 0 % 2 = 0
+            // 1 % 2 = 1
+            if (n % 2 == 1) {
+                ones++;
+            }
+            // Right shift by one
+            n = n >> 1;
+
+            // Same thing, but using bitwise AND with 1 instead
+            // ones += n & 1 == 1 ? 0 : 1;
+            // n >>= 1;
+        }
+        return ones;
+    }
+}
