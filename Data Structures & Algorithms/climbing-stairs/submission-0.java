ded197c150f9d6dd1ec0c3681abd50f088671930
abajrach <arbindrabajracharya@gmail.com>
2026-04-04 13:42:12 -0700

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1; // 1 way to reach step 1
+        dp[2] = 2; // 2 ways to reach step 2. 1,1 and 2 steps
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+}
