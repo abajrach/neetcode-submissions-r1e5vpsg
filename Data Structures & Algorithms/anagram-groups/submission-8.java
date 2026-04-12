5e7a7a217b4c14caf71875b8db830a3ba1c7965c
abajrach <arbindrabajracharya@gmail.com>
2026-04-11 20:23:25 -0700

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.java
@@ -0,0 +1,21 @@
+class Solution {
+    // Hashtable. O()
+    // ["act","pots","tops","cat","stop","hat"]
+    /*
+
+    */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> result = new HashMap<>();
+        for (String s: strs) {
+            int[] count = new int[26];
+            for (char ch: s.toCharArray()) {
+                count[ch - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            result.putIfAbsent(key, new ArrayList<>());
+            result.get(key).add(s);
+        }
+
+        return new ArrayList<>(result.values());
+    }
+}
