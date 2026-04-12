8d3718d7733f7a621ff9ffe78f631bae5550d3e4
abajrach <arbindrabajracharya@gmail.com>
2026-04-11 19:25:09 -0700

    Add: longest-common-prefix - submission-3

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-3.java
@@ -0,0 +1,21 @@
+class Solution {
+     // Horizontal scanning. O (m * n)
+    public String longestCommonPrefix(String[] strs) {
+        // bank, bat, bag, b
+        //        i
+        // prefix = b
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            int j = 0;
+            while (j < Math.min(prefix.length(), strs[i].length())) {
+                if (strs[i].charAt(j) != prefix.charAt(j)) {
+                    break;
+                }
+                j++;
+            }
+            prefix = prefix.substring(0, j);
+        }
+        return prefix;
+    }
+}
