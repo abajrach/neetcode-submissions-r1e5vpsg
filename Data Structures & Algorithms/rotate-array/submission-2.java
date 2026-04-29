4ab2583fdea94bce59979aae9415c4dc3ba4cbd8
abajrach <arbindrabajracharya@gmail.com>
2026-04-29 11:40:40 -0700

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.java
@@ -0,0 +1,17 @@
+class Solution {
+
+    // O(n) for time and space
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        int[] temp = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            // temp[i] = nums[(i + k) % n];
+            temp[(i + k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
