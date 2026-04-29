bd00c512d96cb20dff02651d01c9544e08589e4f
abajrach <arbindrabajracharya@gmail.com>
2026-04-29 11:28:41 -0700

    Add: rotate-array - submission-0

---
+++ Data Structures & Algorithms/rotate-array/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    // Brute force. O(n * k)
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+
+        while (k > 0) {
+            int temp = nums[nums.length - 1];
+            for (int i = nums.length - 1; i > 0; i--) {
+                nums[i] = nums[i - 1];
+            }
+            nums[0] = temp;
+            k--;
+        }
+    }
+}
