47db1a43e11d799f6e685b98b44e4fa5840ef6e1
abajrach <arbindrabajracharya@gmail.com>
2026-04-29 12:22:31 -0700

    Add: rotate-array - submission-6

---
+++ Data Structures & Algorithms/rotate-array/submission-6.java
@@ -0,0 +1,30 @@
+class Solution {
+    /*
+        1) Reverse entire array
+        2) Reverse first k elements
+        3) Reverse rest of the elements
+
+            1 2 3 4 5 6 7 8
+        1)  8 7 6 5 4 3 2 1
+        2)  5 6 7 8 4 3 2 1
+        3)  5 6 7 8 1 2 3 4 
+    */
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
+
+    private void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+}
