b597829f7e780dbd9d945cb52cb7c604e57afbc4
abajrach <arbindrabajracharya@gmail.com>
2026-08-02 15:22:38 -0700

    Add: assign-cookies - submission-1

---
+++ Data Structures & Algorithms/assign-cookies/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0, j = 0;
+        // for (int j = 0; i < g.length && j < s.length; j++) {
+        while (i < g.length && j < s.length) {
+            if (g[i] <= s[j]) {
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
